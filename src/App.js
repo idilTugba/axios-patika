a0feeca008b75fee336c081111e5123dee7aa4e9
idilTugba <idilgolcuk@gmail.com>
2023-02-14 23:55:58 +0300

    load work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import axios from "axios";
 
 function App() {
+
+  const [number, setNumber] = useState(1);
+
+  const userData = async (number) =>{
+    const {data : user} = await axios.get('https://jsonplaceholder.typicode.com/users/'+number);
+    const {data: userPost} = await axios.get('https://jsonplaceholder.typicode.com/posts?userId='+number);
+    console.log(user, userPost);
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={number} onChange={(e)=>setNumber(e.target.value)}/>
+      <button onClick={()=>userData(number)}>GET DATA</button>
     </div>
   );
 }
